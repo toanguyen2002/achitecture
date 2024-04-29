76f0c0587172ea15474b01e2f05b45070a588f62
Quang Toan <toanguyen200220@gmail.com>
2024-04-29 22:06:42 +0700

    update register-service

--- course-Service/src/main/java/com/iuh/courseservice/model/ClassCourse.java
+++ course-Service/src/main/java/com/iuh/courseservice/model/ClassCourse.java
@@ -20,7 +20,6 @@ public class ClassCourse implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idClass;
     private String nameClass;
-
     @OneToMany(mappedBy = "classcourse",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
     private Set<ClassCourseDetail> classCourseDetails;
     @ManyToOne
